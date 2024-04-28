274954927585ac8cd666b1d2bfe1fb55746137c5
Elmo Tabuzo <elmotabuzojr.ust@gmail.com>
2024-04-28 22:39:57 +0800

    Adding Controller Mediatr Query

---
+++ HR.LeaveManagement.API/Controllers/LeaveTypesController.cs
@@ -0,0 +1,56 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        public IMediator _mediator { get; }
+
+        // IMediator was already added because we already inject it into core project services.AddMediatr()..
+        public LeaveTypesController(IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<LeaveTypesController>
+        [HttpGet]
+        public async Task<List<LeaveTypeDto>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypesQuery());
+            return leaveTypes;
+        }
+
+        // GET api/<LeaveTypesController>/5
+        [HttpGet("{id}")]
+        public async Task<LeaveTypeDetailsDto> Get(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailsQuery(id));
+            return leaveType;
+        }
+
+        // POST api/<LeaveTypesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveTypesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveTypesController>/5 
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
