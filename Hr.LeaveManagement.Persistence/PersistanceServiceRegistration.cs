0e1dccdf87c324bf8e80e835c69ff0bb7c60f146
Elmo Tabuzo <elmotabuzojr.ust@gmail.com>
2024-04-28 14:58:50 +0800

    Added EF DB Context and Seed

---
+++ Hr.LeaveManagement.Persistence/PersistanceServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Hr.LeaveManagement.Persistence.DatabseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Persistence
+{
+    public static class PersistanceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HRDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HRDatabaseConnectionString"));
+            });
+
+            return services;
+        }
+    }
+}
