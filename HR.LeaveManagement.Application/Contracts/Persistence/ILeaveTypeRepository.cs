6dfa64d130ae05ac0d14d385fca31c0b16a06913
Elmo Tabuzo <elmotabuzojr.ust@gmail.com>
2024-04-28 15:47:06 +0800

    Added LeaveRequest Implementation

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -2,6 +2,7 @@
 
 namespace HR.LeaveManagement.Application.Contracts.Persistence
 {
+    // Used only for validator
     public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
         Task<bool> IsLeaveTypeUnique(string name);
--- Hr.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ Hr.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Hr.LeaveManagement.Persistence.DatabseContext;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hr.LeaveManagement.Persistence.Repositories
 {
@@ -9,5 +10,34 @@ namespace Hr.LeaveManagement.Persistence.Repositories
         public LeaveRequestRepository(HRDatabaseContext context) : base(context)
         {
         }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails()
+        {
+            var leaveRequests = await _context.LeaveRequests.Include(q => q.LeaveType).ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaverequestWithDetails(string userId)
+        {
+            var leaveRequests = await _context.LeaveRequests.Where(q => q.RequestingEmployeeId == userId)
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
+            return leaveRequest;
+        }
+
+        
+
+ 
     }
 }
